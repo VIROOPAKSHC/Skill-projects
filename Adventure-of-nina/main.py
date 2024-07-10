248368988c8ab6779e4bbc2d8a9b218492b88b64
VIROOPAKSHC <chekuriviroopaksh@gmail.com>
2024-07-10 23:50:48 +0530

    Update-3

--- Adventure-of-nina/main.py
+++ Adventure-of-nina/main.py
@@ -1,13 +1,15 @@
 ### Author - Chekuri Viroopaksh
 
 import time
+import json
 import sys
+import os
 from helper import *
 
 # See the links in the Personal Brave for text based RPGs
 
-commands = []
-definitions = {}
+commands = ["exit","help","save","menu"]
+definitions = {"exit":"Use this command to exit the game","help":"Use this command to get the help menu","save":"Use this command to save the current game state","menu":"Use this command to get to the main menu."}
 current_game_config = {}
 
 def start_new():
@@ -26,6 +28,13 @@ def start_new():
         else:
             username = input("              What is your name?\n(At least 3 and at most 12 characters, cannot contain special characters except *,@)\n>> ")
     print("Hello "+username,end=", ")
+    if (username+".json") in os.listdir():
+        print("You already have a saved game. Do you want to start new?\n1)Yes\n2)No\n")
+        inp = input(">> ")
+        while not(inp in "12"):
+            inp = input("Enter a valid option.\n1)Start new.\n2)Continue from saved.\n>> ")
+        if inp =='2':
+            load_game(username)
     userage = -1
     while True:
         if 3 <= userage <= 100:
@@ -40,15 +49,30 @@ def start_new():
 
     current_game_config["username"] = username
     current_game_config['userage'] = userage
-        
+    current_game_config["actions"] = []
+    current_game_config["states"] = []
 
-def load_game():
-    
-    pass
+def load_game(username=""):
+    f = 0
+    while True:
+        if not(username):
+            inp = input("What's your username?\n>> ")
+        else:
+            inp = username
+        if (inp+'.json') in os.listdir():
+            print("Woohoo! Found your last saved game!!")
+            current_game_config = json.loads(open(inp+'.json').read())
+        else:
+            f = 1
+            print("Sorryy! We couldn't find your game. Are you sure?")
+        
+        if not f:
+            break
 
 def help():
     global commands
     global definitions
+
     print(f"""
 {f'{"`"*135}'}
 
@@ -59,9 +83,11 @@ def help():
     print()
     time.sleep(5)
 
-def save_game(states):
+def save_game():
     # Use JSON files for saving the game
-    pass
+    f = open(current_game_config['username']+'.json','w')
+    f.write(json.dumps(current_game_config))
+
 
 def exit_game(states):
     # Handle saving the game etc. before exiting
@@ -96,6 +122,7 @@ def play():
     global commands
     global current_game_config
     global definitions
+
     states = []
     welcome(states)
     
@@ -108,24 +135,36 @@ def play():
                     break
                 inp = home_screen(states,state='start')
 
-        if inp=='2':
-            current_game_config,game_play = load_game()
-        else:
-            game_play = []
-
+        
+        game_play = True
+        started = False
         while True:
             # Inner loop for playing the actual game
-            if not game_play:
-                game_opening()
+            if game_play:
+                if not started:
+                    game_opening()
+                    started = True
                 print("What do you want to do?")
                 option = input(">> ")
-                if option == 'exit':
-                    print("Thanks for playing the game.")
-                    sys.exit(0)
+                if option in commands:
+                    if option == 'menu':
+                        game_play = False
+                    elif option == 'help':
+                        states.append("Help")
+                        help()
+                    elif option == 'exit':
+                        states.append("Exit")
+                        exit_game(states)
+                    else:
+                        save_game()
+                else:
+                    print(f"{option} command not found! Use help for available commands.\n")
                 # Starting a new game
-
+                
             else:
                 # Continue a game from previous saved point.
+                inp = home_screen(states)
+                menu(states,inp)
                 pass 
             pass
 
